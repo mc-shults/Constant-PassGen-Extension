2f976298f6c600a7dcded2009f132c0d63a6f409
Дмитрий Шулицкий <dmitrii-1997@outlook.com>
2020-01-25 18:11:58 +0300

    Add symbol classes

--- popup/pass_gen.js
+++ popup/pass_gen.js
@@ -1,5 +1,5 @@
 function parseHexString(str) { 
-    var result = [];
+    let result = [];
     while (str.length >= 2) { 
         result.push(parseInt(str.substring(0, 2), 16));
 
@@ -9,27 +9,70 @@ function parseHexString(str) {
     return result;
 }
 
-var letter_count = 'z'.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
-function getChar(value)
-{
-	var index = value % (letter_count + 10)
-	if(index < letter_count){
+//#region SymbolClasses
+
+let lowercaseClass = {
+	size : 'z'.charCodeAt(0) - 'a'.charCodeAt(0) + 1,
+	getSymbol(index) {
 		return String.fromCharCode('a'.charCodeAt(0) + index);
-	} 
-	else {
-		return String.fromCharCode('0'.charCodeAt(0) + index - letter_count);
+	}
+};
+
+let uppercaseClass = {
+	size : 'Z'.charCodeAt(0) - 'A'.charCodeAt(0) + 1,
+	getSymbol(index) {
+		return String.fromCharCode('A'.charCodeAt(0) + index);
+	}
+};
+
+let digitClass = {
+	size : '9'.charCodeAt(0) - '0'.charCodeAt(0) + 1,
+	getSymbol(index) {
+		return String.fromCharCode('0'.charCodeAt(0) + index);
+	}
+};
+
+let specSymbols = ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',',
+	'-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '`', '{', '|',
+	'}', '~'];
+let specialSymbolClass = {
+	size : specSymbols.length,
+	getSymbol(index) {
+		return specSymbols[index];
+	}
+};
+
+//#endregion SymbolClasses
+
+let symbolClasses = [lowercaseClass, digitClass];
+function getChar(value) {
+	console.log(value + '\n');
+	let currentIndex = value;
+	if (currentIndex < 0){
+		return "?";
+	}
+	while (true) {
+		for (let symbolClass of symbolClasses){
+			if (currentIndex < symbolClass.size) {
+				console.log('-----\n');
+				return symbolClass.getSymbol(currentIndex)
+			} else {
+				currentIndex -= symbolClass.size
+			}
+			console.log(currentIndex + ' ananas \n')
+		}
 	}
 }
 
 function calculate(){
-	var srcString = document.getElementById('password').value + document.getElementById('srcString').value;
-	
-	var hexedArray = parseHexString(sha3_256(srcString));
-	var resultString = "";
-	for(var i = 0; i < 8; i++){
+	let srcString = document.getElementById('password').value + document.getElementById('srcString').value;
+
+	let hexedArray = parseHexString(sha3_256(srcString));
+	let resultString = "";
+	for(let i = 0; i < 8; i++){
 		resultString += getChar(hexedArray[i]);
 	}
-	var result = document.getElementById('result');
+	let result = document.getElementById('result');
 	result.value = resultString;
 	result.select();
 	document.execCommand("copy");
@@ -60,8 +103,8 @@ document.addEventListener("click", (e) => {
 
 function logTabs(tabs) {
     let tab = tabs[0]; // Safe to assume there will only be one result
-	var hostname = tab.url.split('/')[2];
-    var hostnameParts = hostname.split('.')
+	let hostname = tab.url.split('/')[2];
+	let hostnameParts = hostname.split('.');
 	document.getElementById('srcString').value = hostnameParts[hostnameParts.length - 2] + "." + hostnameParts[hostnameParts.length - 1];
 }
 
