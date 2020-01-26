944bb8fef025f8aa2ec9d468da53bc6b658525b5
Дмитрий Шулицкий <dmitrii-1997@outlook.com>
2020-01-26 14:29:56 +0300

    use all symbol classes

--- popup/pass_gen.js
+++ popup/pass_gen.js
@@ -8,6 +8,7 @@ let symbolClassMap = {
 	'lowercase': {
 		size: 'z'.charCodeAt(0) - 'a'.charCodeAt(0) + 1,
 		selected: true,
+		used: false,
 		getSymbol(index) {
 			return String.fromCharCode('a'.charCodeAt(0) + index);
 		}
@@ -15,6 +16,7 @@ let symbolClassMap = {
 	'uppercase': {
 		size : 'Z'.charCodeAt(0) - 'A'.charCodeAt(0) + 1,
 		selected: false,
+		used: false,
 		getSymbol(index) {
 			return String.fromCharCode('A'.charCodeAt(0) + index);
 		}
@@ -22,6 +24,7 @@ let symbolClassMap = {
 	'digit': {
 		size : '9'.charCodeAt(0) - '0'.charCodeAt(0) + 1,
 		selected: true,
+		used: false,
 		getSymbol(index) {
 			return String.fromCharCode('0'.charCodeAt(0) + index);
 		}
@@ -29,6 +32,7 @@ let symbolClassMap = {
 	'special-symbol': {
 		size : specSymbols.length,
 		selected: false,
+		used: false,
 		getSymbol(index) {
 			return specSymbols[index];
 		}
@@ -51,7 +55,7 @@ function parseHexString(str) {
 async function digest(str, algorithm) {
 	const encoder = new TextEncoder();
 	const data = encoder.encode(str);
-	const hashBuffer = await crypto.subtle.digest(algorithm, data)
+	const hashBuffer = await crypto.subtle.digest(algorithm, data);
 	return Array.from(new Uint8Array(hashBuffer));
 }
 
@@ -76,9 +80,8 @@ let algorithms = {
 //#endregion Algorithms
 
 const algorithmName = 'SHA3-256';
-const passwordLength = 8;
+const passwordLength = 4;
 function getChar(value) {
-	console.log(value + '\n');
 	let currentIndex = value;
 	if (currentIndex < 0){
 		return "?";
@@ -90,27 +93,46 @@ function getChar(value) {
 				continue;
 			}
 			if (currentIndex < symbolClass.size) {
-				console.log('-----\n');
+				symbolClassMap[symbolClassPair[0]].used = true;
 				return symbolClass.getSymbol(currentIndex)
 			} else {
 				currentIndex -= symbolClass.size
 			}
-			console.log(currentIndex)
 		}
 	}
-	console.error("Endless symbol loop!");
 }
 
-async function calculate(){
-	let srcString = document.getElementById('password').value
-		          + document.getElementById('site-string').value
-	              + document.getElementById('login-string').value;
-
+async function generatePassword(srcString, iteration) {
+	console.log(iteration);
+	console.log(srcString);
+	if (iteration > 1024) {
+		return "";
+	}
+	for (let key in symbolClassMap) {
+		symbolClassMap[key].used = false;
+	}
 	let hexedArray = await algorithms[algorithmName](srcString);
 	let resultString = "";
 	for(let i = 0; i < passwordLength; i++){
 		resultString += getChar(hexedArray[i]);
 	}
+	let allUsed = true;
+	for (let key in symbolClassMap) {
+		allUsed &= !symbolClassMap[key].selected || symbolClassMap[key].used;
+	}
+	if (allUsed) {
+		return resultString;
+	} else {
+		console.log(resultString);
+		return generatePassword(srcString + hexedArray.reduce((accum, val) => accum + ("00" + val.toString(16)).slice(-2) ,""), iteration+1)
+	}
+}
+
+async function updateResult(){
+	let srcString = document.getElementById('password').value
+		+ document.getElementById('site-string').value
+		+ document.getElementById('login-string').value;
+	let resultString = await generatePassword(srcString, 0);
 	let result = document.getElementById('result');
 	result.value = resultString;
 }
@@ -124,7 +146,7 @@ function selectResult () {
 dataChanged = function(event) {
 	event.preventDefault();
 	if (event.keyCode === 13) {
-		calculate().then(() => {
+		updateResult().then(() => {
 			selectResult ()
 		});
 	}
@@ -205,7 +227,7 @@ initHideButton("toggle-result", "hide-result");
 
 document.addEventListener("click", (e) => {
 	if (e.target.classList.contains("ok")) {
-		calculate();
+		updateResult();
 	} else if(e.target.classList.contains("open-settings")) {
 		showSettings();
 		e.target.classList.remove("open-settings");
